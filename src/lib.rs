ef742e127c562b9ac5309d1d6291d523c1376d57
Peter Kubiak <peter.s.kubiak@bath.edu>
2025-04-07 01:27:38 +0100

    Begin again on defining postmaster

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,33 @@ pub mod agent;
 pub mod postmaster;
 
 #[macro_export]
-#[crabtime::function]
-fn init_postmaster(pattern!($address_type:ty, $message_type:ty): _) {
-    crabtime::output! {println!("Hello")}
+macro_rules! init_postmaster {
+    ($address:ty, $message:ty) => {
+        mod postmaster_internal {
+            use embassy_sync::channel::DynamicSender;
+
+            const ADDRESS_COUNT: usize = core::mem::variant_count::<$address>();
+
+            struct Postmaster<'a> {
+                senders:
+                    [Option<DynamicSender<'a, <Self as PostmasterTypes>::Message>>; ADDRESS_COUNT],
+            }
+
+            unsafe impl Sync for Postmaster<'_> {}
+
+            trait PostmasterTypes {
+                type Address;
+                type Message;
+            }
+
+            impl PostmasterTypes for Postmaster<'_> {
+                type Address = $address;
+                type Message = $message;
+            }
+
+            static POSTMASTER: Postmaster = Postmaster {
+                senders: [None; ADDRESS_COUNT],
+            };
+        }
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+#![feature(variant_count)]
+use embassy_sync::channel::DynamicSender;
+
 enum Address {}
+
 enum Message {}
 fn main() {
-    post_haste::init_postmaster!(Address, Message);
+    post_haste::init_postmaster!(crate::Address, crate::Message);
+}
+
+struct Postmaster<'a> {
+    senders: [DynamicSender<'a, <Postmaster<'a> as Pm>::Message>],
+}
+
+trait Pm {
+    type Address;
+    type Message;
+}
+
+impl Pm for Postmaster<'_> {
+    type Address = Address;
+    type Message = Message;
 }
