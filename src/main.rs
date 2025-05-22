cfa6f1eda63c5b41b3dd7b1863fe07cc4b12282f
Peter Kubiak <peter.s.kubiak@bath.edu>
2025-05-22 01:55:15 +0100

    Add suitable derives for addresses and message payloads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![feature(variant_count)]
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum Addresses {}
+
+#[derive(Clone, Debug, PartialEq)]
 enum MessagePayload {}
 
 post_haste::init_postmaster!(Addresses, MessagePayload);
