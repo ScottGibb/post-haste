6ce449a99c5668be2e1573477733eb25f8495a4b
Peter Kubiak <peter.s.kubiak@bath.edu>
2025-08-25 18:23:28 +0100

    [WIP] Begin adding docstrings to tokio_showcase.rs

--- examples/tokio_showcase.rs
+++ examples/tokio_showcase.rs
@@ -1,17 +1,26 @@
+//! The purpose of this example is to demonstrate some useful patterns and concepts which take advantage of the post-haste library to great effect.
+//! The logic of this example is very similar to that of the [tokio_basic](tokio_basic.rs)example: simple Agents are created which respond to a "hello" message in kind.
+//! "Hello" messages are then sent from the main task to the Agents, with the source address given as one of the other Agents.
+//! This will prompt the Agent to respond with its own "hello" back to the source, initiating an infinite loop.
+
 #![feature(variant_count)]
-
-// TODO: Add docstrings to explain the concepts laid out here
-
 use core::time::Duration;
 
 use polite_agent::{PoliteAgent, PoliteAgentConfig, PoliteAgentMessage};
 use post_haste::init_postmaster;
 use tokio::time::sleep;
 
+/// This enum describes the messages used by the system.
+/// This could be arranged as a single enum of all possible messages, however I prefer to group messages by the actor they are associated with.
+/// In this case, as there is only one actor there is only one Payload variant.
 enum Payloads {
+    /// This variant covers all messages associated with the Polite Agent.
     PoliteMessage(PoliteAgentMessage),
 }
 
+/// This enum provides all Agent addresses.
+/// Each Agent must be assigned a unique address upon registration with the Postmaster.
+/// As indicated by the signature of the Agent trait's `run()` method, Agents are expected to live for the lifetime of the application. This ensures that addresses are always valid and messages aren't accidentally sent to an unoccupied address.
 #[derive(Debug, Clone, Copy)]
 enum Address {
     AgentA,
@@ -19,74 +28,26 @@ enum Address {
     AgentC,
 }
 
+/// This macro call generates all the functionality associated with the Postmaster.
+/// The Address and Payload enum types are provided as arguments, as they are required for the functionality of the Postmaster.
 init_postmaster!(Address, Payloads);
 
-#[tokio::main]
-async fn main() {
-    register_agent!(
-        AgentA,
-        PoliteAgent,
-        PoliteAgentConfig {
-            custom_greeting: None,
-            reply_delay: Duration::from_secs(1)
-        }
-    );
-    register_agent!(
-        AgentB,
-        PoliteAgent,
-        PoliteAgentConfig {
-            custom_greeting: Some("Good day!".to_string()),
-            reply_delay: Duration::from_secs(2)
-        }
-    );
-    register_agent!(
-        AgentC,
-        PoliteAgent,
-        PoliteAgentConfig {
-            custom_greeting: Some("Ahoy!".to_string()),
-            reply_delay: Duration::from_secs(3)
-        }
-    );
-
-    postmaster::send(
-        Address::AgentA,
-        Address::AgentB,
-        Payloads::PoliteMessage(PoliteAgentMessage::Hello),
-    )
-    .await
-    .unwrap();
-    postmaster::send(
-        Address::AgentB,
-        Address::AgentC,
-        Payloads::PoliteMessage(PoliteAgentMessage::Hello),
-    )
-    .await
-    .unwrap();
-    postmaster::send(
-        Address::AgentC,
-        Address::AgentA,
-        Payloads::PoliteMessage(PoliteAgentMessage::Hello),
-    )
-    .await
-    .unwrap();
-
-    loop {
-        sleep(Duration::from_secs(60)).await;
-        let diagnostics = postmaster::get_diagnostics();
-        println!("Postmaster diagnostics:");
-        println!("Messages sent: {}", diagnostics.messages_sent);
-        println!("Send failures: {}", diagnostics.send_failures);
-    }
-}
-
+/// This module provides all functionality and types associated with the Polite Agent.
+/// Usually this would be in its own file, however here it is presented as a module so that the example is a single, self-reliant file.
 mod polite_agent {
     use crate::{Address, Payloads, postmaster};
     use core::time::Duration;
     use post_haste::agent::Agent;
 
+    /// This struct acts as the "instance" of the Agent, and is ususally used to hold state and configuration information
     pub(super) struct PoliteAgent {
+        /// Storing the address of the agent as a member field is optional, however it means that `self.address` can be used as the source when sending messages.
+        /// In cases with multiple instances of an Agent type are running concurrently, this becomes very necessary.
         address: Address,
+        /// Our PoliteAgent can be configured with an optional custom greeting, rather than having to rely on the standard "hello".
         greeting: Option<String>,
+        /// In order to avoid spamming stdout with messages, we also configure the PoliteAgent with a delay.
+        /// When a message is received, it will wait for this amount of time before sending its reply.
         reply_delay: Duration,
     }
 
@@ -181,3 +142,62 @@ mod polite_agent {
         }
     }
 }
+
+#[tokio::main]
+async fn main() {
+    register_agent!(
+        AgentA,
+        PoliteAgent,
+        PoliteAgentConfig {
+            custom_greeting: None,
+            reply_delay: Duration::from_secs(1)
+        }
+    );
+    register_agent!(
+        AgentB,
+        PoliteAgent,
+        PoliteAgentConfig {
+            custom_greeting: Some("Good day!".to_string()),
+            reply_delay: Duration::from_secs(2)
+        }
+    );
+    register_agent!(
+        AgentC,
+        PoliteAgent,
+        PoliteAgentConfig {
+            custom_greeting: Some("Ahoy!".to_string()),
+            reply_delay: Duration::from_secs(3)
+        }
+    );
+
+    postmaster::send(
+        Address::AgentA,
+        Address::AgentB,
+        Payloads::PoliteMessage(PoliteAgentMessage::Hello),
+    )
+    .await
+    .unwrap();
+    postmaster::send(
+        Address::AgentB,
+        Address::AgentC,
+        Payloads::PoliteMessage(PoliteAgentMessage::Hello),
+    )
+    .await
+    .unwrap();
+    postmaster::send(
+        Address::AgentC,
+        Address::AgentA,
+        Payloads::PoliteMessage(PoliteAgentMessage::Hello),
+    )
+    .await
+    .unwrap();
+
+    loop {
+        sleep(Duration::from_secs(60)).await;
+        let diagnostics = postmaster::get_diagnostics();
+        println!("Postmaster diagnostics:");
+        println!("Messages sent: {}", diagnostics.messages_sent);
+        println!("Send failures: {}", diagnostics.send_failures);
+    }
+}
+
